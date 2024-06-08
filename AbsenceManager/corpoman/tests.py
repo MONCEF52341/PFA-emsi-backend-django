96a574ed0ab2ce3646fcbea135465d90c9074571
Moncef <141020997+MONCEF52341@users.noreply.github.com>
2024-06-08 21:14:32 +0100

    test github actions

--- AbsenceManager/corpoman/tests.py
+++ AbsenceManager/corpoman/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import LieuTravail
+from django.urls import resolve
 
-# Create your tests here.
+class LieuTravailTestCase(TestCase):
+    def setUp(self):
+        # Créer des objets de test ici si nécessaire
+        LieuTravail.objects.create(
+            designation="OpenSpaceTest",
+            description="Ceci est un test d'OpenSpace",
+            type="OpenSpace",
+            capacite=20
+        )
+
+    def test_lieu_travail(self):
+        # Récupérer l'objet créé dans setUp()
+        lieu_travail_test = LieuTravail.objects.get(designation="OpenSpaceTest")
+        # Vérifier si les attributs de l'objet sont corrects
+        self.assertEqual(lieu_travail_test.description, "Ceci est un test d'OpenSpace")
+        self.assertEqual(lieu_travail_test.type, "OpenSpace")
+        self.assertEqual(lieu_travail_test.capacite, 20)
+
+
+class URLTestCase(TestCase):
+    def test_all_urls_return_200(self):
+        for url_pattern in urlpatterns:
+            # Vérifie si l'élément de urlpatterns est une URL
+            if hasattr(url_pattern, 'pattern'):
+                # Obtient l'URL à partir du pattern
+                url = url_pattern.pattern.regex.pattern
+                # Effectue une requête GET à l'URL
+                response = self.client.get(url)
+                # Vérifie que la réponse est 200 OK
+                self.assertEqual(response.status_code, 200, f"{url} a retourné un code de statut différent de 200")
